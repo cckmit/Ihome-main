d6014b518d0c52208551ab1f73aba413de57045d
POLYSWEET\zhangyuchi <zhangyuchi@polyihome.com>
2020-09-30 14:34:47 +0800

    common，main项目配置api生成脚本

---
+++ ihome/ihome-main/src/api/base.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: zyc
+ * @Date: 2020-07-08 10:41:39
+ * @LastEditors: zyc
+ * @LastEditTime: 2020-08-26 10:15:13
+ */
+export { IRes, IPageInfo, IPageBase } from 'ihome-common/util/api/base.d'
