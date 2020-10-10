ac306a7ba66eda8e4507fdb66dec5000ef547a6c
wanweiqiang05531 <wanweiqiang055361@polysweet.local>
2020-10-10 18:28:35 +0800

    合并代码导致路由丢失

--- ihome/ihome-sales/src/router/index.ts
+++ ihome/ihome-sales/src/router/index.ts
@@ -4,7 +4,7 @@
  * @Author: zyc
  * @Date: 2020-06-22 11:10:43
  * @LastEditors: wwq
- * @LastEditTime: 2020-10-10 18:18:31
+ * @LastEditTime: 2020-10-10 18:27:14
  */
 import Vue from 'vue'
 import VueRouter, { RouteConfig, Route } from 'vue-router'
@@ -236,7 +236,47 @@ const routes: Array<RouteConfig> = [
     path: '/developers',
     meta: { title: '开发商管理', icon: null },
     redirect: '/developers/list',
+    component: Layout,
+    children: [
+      {
+        path: 'list',
+        name: 'developerList',
+        component: () => import('../views/developer/list.vue'),
+        meta: { title: '开发商列表', icon: 'form' }
+      },
+      {
+        path: 'edit',
+        name: 'developerEdit',
+        component: () => import('../views/developer/edit.vue'),
+        meta: { title: '开发商信息录入修改', icon: 'form' }
+      },
+      {
+        path: 'change',
+        name: 'developerChange',
+        component: () => import('../views/developer/edit.vue'),
+        meta: { title: '开发商信息变更', icon: 'form' }
+      },
+      {
+        path: 'details',
+        name: 'developerDetails',
+        component: () => import('../views/developer/details.vue'),
+        meta: { title: '开发商信息详情', icon: 'form' }
+      },
+      {
+        path: 'check',
+        name: 'developerCheck',
+        component: () => import('../views/developer/details.vue'),
+        meta: { title: '开发商信息审核', icon: 'form' }
+      },
+      {
+        path: 'revocation',
+        name: 'developerRevocation',
+        component: () => import('../views/developer/details.vue'),
+        meta: { title: '开发商信息撤回', icon: 'form' }
+      }
+    ]
   }
+
 ]
 const router = new VueRouter({
   mode: 'history',
