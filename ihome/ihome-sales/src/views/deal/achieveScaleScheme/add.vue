1f7d623b0b9b48b297755176ae8f58d928b15afc
lushijian17361 <lushijian072@chinasoftinc.com>
2020-11-12 07:45:59 +0800

    修改添加功能

--- ihome/ihome-sales/src/views/deal/achieveScaleScheme/add.vue
+++ ihome/ihome-sales/src/views/deal/achieveScaleScheme/add.vue
@@ -65,6 +65,7 @@
           <el-form-item label="分销同步总包">
             <el-select
               v-model="postData.isSame"
+              @change="handleChangeIsSame"
               clearable
               placeholder="请选择"
               class="width--100">
@@ -109,22 +110,36 @@
           </el-form-item>
         </el-col>
       </el-row>
-      <el-form-item label="" label-width="0" style="padding-left: 20px;">
-        <div class="add-all-wrapper">
-          <el-button type="success" @click="addTotalPackage">添加总包</el-button>
-          <el-button type="success" @click="addDistribution">添加分销</el-button>
-        </div>
-        <el-table
-          class="ih-table"
-          :max-height="500"
-          :data="addList">
-          <el-table-column prop="typeName" label="类别" min-width="100"></el-table-column>
-          <el-table-column prop="role" label="角色" min-width="120">
-            <template slot-scope="scope">
+    </el-form>
+    <el-form
+      :model="tableModel"
+      :rules="tableModel.rules"
+      ref="tableForm"
+      label-width="0"
+      class="demo-ruleForm form-padding-20">
+      <div class="add-all-wrapper">
+        <el-button type="success" @click="addTotalPackage">添加总包</el-button>
+        <el-button type="success" @click="addDistribution">添加分销</el-button>
+      </div>
+      <el-table
+        class="ih-table"
+        :max-height="500"
+        :data="tableModel.tableData">
+        <el-table-column prop="typeName" label="类别" min-width="100">
+          <template slot-scope="scope">
+            <el-form-item>{{scope.row.typeName}}</el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column prop="role" label="角色" min-width="120">
+          <template slot-scope="scope">
+            <el-form-item
+              :prop="'tableData.' + scope.$index + '.role'"
+              :rules='tableModel.rules.role'>
               <el-select
                 v-model="scope.row.role"
                 @change="handleChangeRole($event, scope)"
                 clearable
+                :disabled="postData.isSame === 'yes' && scope.row.type === 'Distri'"
                 placeholder="请选择角色"
                 class="width--100">
                 <el-option
@@ -134,24 +149,40 @@
                   :value="item.code"
                 ></el-option>
               </el-select>
-            </template>
-          </el-table-column>
-          <el-table-column prop="ratio" label="拆分比例 (%)" min-width="120">
-            <template slot-scope="scope">
-              <el-input-number
-                class="input-number-left"
+            </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column prop="ratio" label="拆分比例 (%)" min-width="120">
+          <template slot-scope="scope">
+            <!--              <el-input-number-->
+            <!--                class="input-number-left"-->
+            <!--                v-model="scope.row.ratio"-->
+            <!--                @change.native="handleChangeRatio"-->
+            <!--                :min="0" :max="100"-->
+            <!--                :precision="2"-->
+            <!--                :controls="false"-->
+            <!--                placeholder="拆分比例"></el-input-number>-->
+            <el-form-item
+              :prop="'tableData.' + scope.$index + '.ratio'"
+              :rules='tableModel.rules.ratio'>
+              <el-input
+                @keyup.native="handleChangeRatio(scope)"
+                clearable
+                :disabled="postData.isSame === 'yes' && scope.row.type === 'Distri'"
                 v-model="scope.row.ratio"
-                :min="0" :max="100"
-                :precision="2"
-                :controls="false"
-                placeholder="拆分比例"></el-input-number>
-            </template>
-          </el-table-column>
-          <el-table-column prop="missingRole" label="角色缺失处理方式" min-width="120">
-            <template slot-scope="scope">
+                placeholder="拆分比例"></el-input>
+            </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column prop="missingRole" label="角色缺失处理方式" min-width="120">
+          <template slot-scope="scope">
+            <el-form-item
+              :prop="'tableData.' + scope.$index + '.missingRole'"
+              :rules='tableModel.rules.missingRole'>
               <el-select
                 v-model="scope.row.missingRole"
                 clearable
+                :disabled="postData.isSame === 'yes' && scope.row.type === 'Distri'"
                 placeholder="角色缺失处理方式"
                 class="width--100">
                 <el-option
@@ -162,31 +193,36 @@
                   :disabled="scope.row.role === item.code"
                 ></el-option>
               </el-select>
-            </template>
-          </el-table-column>
-          <el-table-column prop="remarks" label="备注" min-width="160">
-            <template slot-scope="scope">
+            </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column prop="remarks" label="备注" min-width="160">
+          <template slot-scope="scope">
+            <el-form-item>
               <el-input
                 v-model="scope.row.remarks"
                 clearable
+                :disabled="postData.isSame === 'yes' && scope.row.type === 'Distri'"
                 placeholder="备注"
               ></el-input>
-            </template>
-          </el-table-column>
-          <el-table-column fixed="right" label="操作" width="100">
-            <template slot-scope="scope">
+            </el-form-item>
+          </template>
+        </el-table-column>
+        <el-table-column fixed="right" label="操作" width="100">
+          <template slot-scope="scope">
+            <el-form-item>
               <el-link
                 class="margin-right-10"
                 type="primary"
                 @click.native.prevent="deleteAdd(scope)"
               >删除
               </el-link>
-            </template>
-          </el-table-column>
-        </el-table>
-      </el-form-item>
+            </el-form-item>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-form>
-    <div class="text-center">
+    <div class="text-center text-margin-top">
       <el-button type="success" @click="save()">保存</el-button>
       <el-button type="primary" @click="cancel()">取消</el-button>
     </div>
@@ -220,13 +256,13 @@
   })
   export default class AchieveScaleSchemeAdd extends Vue {
     postData: any = {
-      modelId: null,
-      contType: null,
-      isMarketProject: null,
-      isSame: null,
-      remarks: null,
-      achievePropertyTypeList: [], // 物业类型
-      achieveProjectList: [], // 关联项目
+      modelId: null, // 业务模式ID
+      contType: null, // 合同类型
+      isMarketProject: null, // 是否市场化
+      isSame: null, // 是否同步
+      remarks: null, // 备注
+      achievePropertyTypeList: [], // 物业类型[{propertyType: ""}]
+      achieveProjectList: [], // 关联项目[{projectId: ""}]
       achieveScaleConfigList: [] // 业绩比例配置
     };
     rules: any = {
@@ -234,6 +270,20 @@
         {required: true, message: "业务模式必选", trigger: "change"},
       ]
     };
+    tableModel: any = {
+      rules: {
+        role: [
+          {required: true, message: "业务模式必选", trigger: "blur"},
+        ],
+        ratio: [
+          {required: true, message: "业务模式必选", trigger: "blur"},
+        ],
+        missingRole: [
+          {required: true, message: "业务模式必选", trigger: "blur"},
+        ]
+      },
+      tableData: []
+    }
     id: any = null;
     companyId: any = null;
     dialogAdd: any = false;
@@ -241,17 +291,37 @@
     totalPackageList: any = []; // 总包
     distributionList: any = []; // 分销
 
-    // 计算属性-获取业绩比例配置列表数据
-    get addList() {
-      return this.totalPackageList.concat(this.distributionList);
-    }
-
     async created() {
       this.id = this.$route.query.id; // 业绩比例方案
       this.companyId = this.$route.query.companyId; // 分公司id
       if (this.id) {
         await this.initGetInfo()
       }
+    }
+
+    // 分销同步总包逻辑
+    handleChangeIsSame(value: any) {
+      console.log('value', value);
+      if (value === 'yes') {
+        this.distributionList = this.totalPackageList;
+        this.distributionList.forEach((item: any) => {
+          item.type = "Distri";
+          item.typeName = "分销";
+        })
+      } else if (value === 'no') {
+        console.log('value', value);
+        this.distributionList = (this as any).$tool.deepClone(this.distributionList);
+      }
+    }
+
+    // 修改比例
+    handleChangeRatio(scope: any) {
+      // console.log(scope);
+      scope.row.ratio = scope.row.ratio.replace(/[^\d.]/g, ""); // 清除"数字"和以外的字符
+      scope.row.ratio = scope.row.ratio.replace(/^\./g, ""); // 验证第一个字符是不是数字
+      scope.row.ratio = scope.row.ratio.replace(/\.{2,}/g, "."); // 只保留第一个"."
+      scope.row.ratio = scope.row.ratio.replace(".", "$#$").replace(/\./g, "").replace("$#$", ".");
+      scope.row.ratio = scope.row.ratio.replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3'); // 只能输入两个小数
     }
 
     // 初始化编辑页面
@@ -315,7 +385,7 @@
     // 新增总包
     async addTotalPackage() {
       let obj = {
-        customizeId: new Date().getTime() + this.totalPackageList.length, // 自定义id，主要用于删除
+        customizeId: new Date().getTime() + this.tableModel.tableData.length, // 自定义id，主要用于删除
         type: "TotalBag",
         typeName: "总包",
         role: null,
@@ -323,13 +393,30 @@
         missingRole: null,
         remarks: null
       }
-      this.totalPackageList.push(obj);
+      if (this.tableModel.tableData && this.tableModel.tableData.length > 0) {
+        let lastIndex = null;
+        this.tableModel.tableData.forEach((data: any, index: any) => {
+          if (data.type === 'TotalBag') {
+            lastIndex = index;
+          }
+        })
+        if (lastIndex !== null) {
+          this.tableModel.tableData.splice(lastIndex + 1, 0, obj)
+        } else {
+          this.tableModel.tableData.unshift(obj);
+        }
+      } else {
+        this.tableModel.tableData.push(obj);
+      }
+      // this.$nextTick(() => {
+      //   (this as any).$refs.tableForm.clearValidate(); // 清空校验
+      // })
     }
 
     // 新增分销
     async addDistribution() {
       let obj = {
-        customizeId: new Date().getTime() + this.totalPackageList.length, // 自定义id，主要用于删除
+        customizeId: new Date().getTime() + this.tableModel.tableData.length, // 自定义id，主要用于删除
         type: "Distri",
         typeName: "分销",
         role: null,
@@ -337,7 +424,10 @@
         dealByMissing: null,
         remarks: null
       }
-      this.distributionList.push(obj);
+      this.tableModel.tableData.push(obj);
+      // this.$nextTick(() => {
+      //   (this as any).$refs.tableForm.clearValidate();
+      // })
     }
 
     // 删除总包/分销
@@ -345,21 +435,10 @@
       console.log(scope);
       try {
         await this.$confirm("是否确定删除?", "提示");
-        if (scope.row.type) {
-          switch (scope.row.type) {
-            // 删除总包
-            case "TotalBag":
-              this.totalPackageList = this.totalPackageList.filter((list: any) => {
-                return list.customizeId !== scope.row.customizeId;
-              })
-              break;
-            // 删除分销
-            case "Distri":
-              this.distributionList = this.distributionList.filter((list: any) => {
-                return list.customizeId !== scope.row.customizeId;
-              })
-              break;
-          }
+        if (this.tableModel.tableData && this.tableModel.tableData.length > 0) {
+          this.tableModel.tableData = this.tableModel.tableData.filter((data: any) => {
+            return data.customizeId !== scope.row.customizeId;
+          })
         }
         this.$message({
           type: "success",
@@ -373,7 +452,7 @@
     // 改变角色选项时的逻辑
     handleChangeRole(value: any, scope: any) {
       // console.log('eeee', value);
-      // console.log('scope', scope);
+      console.log('scope', scope);
       if (value) {
         if (value === scope.row.missingRole) {
           scope.row.missingRole = null;
@@ -445,5 +524,13 @@
     /deep/ .el-input__inner {
       text-align: left;
     }
+  }
+
+  .form-padding-20 {
+    padding-left: 20px;
+  }
+
+  .text-margin-top {
+    margin-top: 20px;
   }
 </style>
