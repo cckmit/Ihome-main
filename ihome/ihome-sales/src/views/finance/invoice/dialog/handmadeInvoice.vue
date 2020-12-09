b513cab53c2408d35b57439cb8ccfb1077702699
yangweili <963492977@qq.com>
2020-12-09 09:54:25 +0800

    feat: 手工开票弹窗

---
+++ ihome/ihome-sales/src/views/finance/invoice/dialog/handmadeInvoice.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Description: 手工开票录入
+ * @version: 
+ * @Author: ywl
+ * @Date: 2020-12-08 21:04:03
+ * @LastEditors: ywl
+ * @LastEditTime: 2020-12-08 21:09:37
+-->
+<template>
+  <el-dialog
+    v-dialogDrag
+    :visible.sync="dialogVisible"
+    :close-on-click-modal="false"
+    :close-on-press-escape="false"
+    class="text-left"
+  ></el-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Handadel extends Vue {
+  dialogVisible = true;
+}
+</script>
