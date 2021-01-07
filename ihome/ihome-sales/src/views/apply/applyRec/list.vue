9fbbfe108700e988621ed346cf52bdc4ea55f5d2
yangweili <963492977@qq.com>
2021-01-07 10:35:58 +0800

    feat: 请佣管理模块

---
+++ ihome/ihome-sales/src/views/apply/applyRec/list.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description: file content
+ * @version: 
+ * @Author: ywl
+ * @Date: 2021-01-07 10:29:38
+ * @LastEditors: ywl
+ * @LastEditTime: 2021-01-07 10:35:09
+-->
+<template>
+  <IhPage></IhPage>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import PaginationMixin from "../../../mixins/pagination";
+
+@Component({
+  mixins: [PaginationMixin],
+})
+export default class ApplyRecList extends Vue {}
+</script>
