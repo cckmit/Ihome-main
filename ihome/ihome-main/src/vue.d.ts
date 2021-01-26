75e2b3dcd66c24f4fdaf8ad010bd7c0d69fcf04c
POLYSWEET\zhangyuchi <zhangyuchi@polyihome.com>
2021-01-26 17:22:30 +0800

    优化vue.d.ts

--- ihome/ihome-main/src/vue.d.ts
+++ ihome/ihome-main/src/vue.d.ts
@@ -4,15 +4,14 @@
  * @Author: zyc
  * @Date: 2020-06-28 17:57:27
  * @LastEditors: zyc
- * @LastEditTime: 2021-01-26 10:35:19
+ * @LastEditTime: 2021-01-26 17:21:37
  */
 
 import 'ihome-common/vue.d'
 import 'ihome-common/app/all.d'
 import Vue from "vue";
 import { ToolInterface } from 'ihome-common/util/tool'
-import { RoleToolInterface } from 'ihome-common/util/roleTool'
-import { MathInterface } from 'ihome-common/util/math'
+
 declare module "vue/types/vue" {
     interface Vue {
         /**工具类
@@ -20,16 +19,7 @@ declare module "vue/types/vue" {
          * @return: 
          */
         $tool: ToolInterface;
-         /**工具类
-         * @param {type} 
-         * @return: 
-         */
-        $roleTool: RoleToolInterface;
-        /**运算
-         * @param {*}
-         * @return {*}
-         */
-        $math: MathInterface;
+     
 
     }
 }
--- ihome/ihome-system/src/vue.d.ts
+++ ihome/ihome-system/src/vue.d.ts
@@ -4,14 +4,14 @@
  * @Author: zyc
  * @Date: 2020-06-28 17:57:27
  * @LastEditors: zyc
- * @LastEditTime: 2021-01-26 10:33:34
+ * @LastEditTime: 2021-01-26 17:19:36
  */
 
 import 'ihome-common/vue.d'
 import 'ihome-common/app/all.d'
 import Vue from "vue";
 import { ToolInterface } from 'ihome-common/util/tool'
-import { RoleToolInterface } from 'ihome-common/util/roleTool'
+
 import { MathInterface } from 'ihome-common/util/math'
 declare module "vue/types/vue" {
     interface Vue {
@@ -20,11 +20,7 @@ declare module "vue/types/vue" {
          * @return: 
          */
         $tool: ToolInterface;
-         /**工具类
-         * @param {type} 
-         * @return: 
-         */
-        $roleTool: RoleToolInterface;
+        
         /**运算
          * @param {*}
          * @return {*}
