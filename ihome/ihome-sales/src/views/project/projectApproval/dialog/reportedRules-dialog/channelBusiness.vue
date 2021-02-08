bbee9f63d5d9774aa7d2c5ad435df6189cbe847d
POLYSWEET\zhongguangneng06381 <799762497@qq.com>
2021-02-08 19:51:41 +0800

    提交

--- ihome/ihome-sales/src/views/project/projectApproval/dialog/reportedRules-dialog/channelBusiness.vue
+++ ihome/ihome-sales/src/views/project/projectApproval/dialog/reportedRules-dialog/channelBusiness.vue
@@ -59,7 +59,7 @@
           </el-form-item>
         </el-col>
         <el-col :span="8">
-          <el-form-item label="状态">
+          <el-form-item hidden="true" label="状态">
             <el-select
               v-model="queryPageParameters.status"
               clearable
@@ -85,13 +85,13 @@
           </el-form-item>
         </el-col>
       </el-row>
-      <el-row>
+      <!-- <el-row>
         <el-col :span="8">
           <el-form-item label="省市区">
             <IhCascader v-model="provinceOption"></IhCascader>
           </el-form-item>
         </el-col>
-      </el-row>
+      </el-row> -->
     </el-form>
     <div class="margin-left-80">
       <el-button
