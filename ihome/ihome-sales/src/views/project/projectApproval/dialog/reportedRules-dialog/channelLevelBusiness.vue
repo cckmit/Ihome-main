6660ed306bc6f2f431d85fc64c88942de206fb03
wanweiqiang05531 <wanweiqiang055361@polysweet.local>
2021-02-20 17:41:36 +0800

    bug修复

--- ihome/ihome-sales/src/views/project/projectApproval/dialog/reportedRules-dialog/channelLevelBusiness.vue
+++ ihome/ihome-sales/src/views/project/projectApproval/dialog/reportedRules-dialog/channelLevelBusiness.vue
@@ -4,7 +4,7 @@
  * @Author: wwq
  * @Date: 2020-08-13 11:40:10
  * @LastEditors: wwq
- * @LastEditTime: 2021-01-27 10:39:54
+ * @LastEditTime: 2021-02-20 17:10:27
 -->
 <template>
   <el-dialog
@@ -13,60 +13,60 @@
     :close-on-click-modal="false"
     :close-on-press-escape="false"
     :before-close="cancel"
-    width="80%"
-    class="text-left dialog-table"
+    width="85%"
+    class="text-left channelLeveLDialog"
     :title="`渠道商等级信息`"
     :append-to-body="true"
   >
-      <el-form
-        ref="form"
-        label-width="100px"
-      >
-        <el-row>
-          <el-col :span="8">
-            <el-form-item label="渠道商名称">
-              <IhSelectPageByChannel
-                v-model="queryPageParameters.channelId"
-                clearable
-                placeholder="渠道商名称"
-              ></IhSelectPageByChannel>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
-            <el-form-item label="渠道等级">
-              <el-select
-                v-model="queryPageParameters.channelGrade"
-                clearable
-                placeholder="渠道等级"
-                class="width--100"
-              >
-                <el-option
-                  v-for="item in $root.dictAllList('ChannelLevel')"
-                  :key="item.code"
-                  :label="item.name"
-                  :value="item.code"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
-            <el-form-item label="城市等级">
-              <el-select
-                v-model="queryPageParameters.cityGrade"
-                clearable
-                placeholder="城市等级"
-                class="width--100"
-              >
-                <el-option
-                  v-for="item in $root.dictAllList('CityLevel')"
-                  :key="item.code"
-                  :label="item.name"
-                  :value="item.code"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-          <!-- <el-col :span="8">
+    <el-form
+      ref="form"
+      label-width="100px"
+    >
+      <el-row>
+        <el-col :span="8">
+          <el-form-item label="渠道商名称">
+            <IhSelectPageByChannel
+              v-model="queryPageParameters.channelId"
+              clearable
+              placeholder="渠道商名称"
+            ></IhSelectPageByChannel>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="渠道等级">
+            <el-select
+              v-model="queryPageParameters.channelGrade"
+              clearable
+              placeholder="渠道等级"
+              class="width--100"
+            >
+              <el-option
+                v-for="item in $root.dictAllList('ChannelLevel')"
+                :key="item.code"
+                :label="item.name"
+                :value="item.code"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="城市等级">
+            <el-select
+              v-model="queryPageParameters.cityGrade"
+              clearable
+              placeholder="城市等级"
+              class="width--100"
+            >
+              <el-option
+                v-for="item in $root.dictAllList('CityLevel')"
+                :key="item.code"
+                :label="item.name"
+                :value="item.code"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <!-- <el-col :span="8">
             <el-form-item label="业务开展省市">
               <IhCascader
                 :level="2"
@@ -77,12 +77,12 @@
               />
             </el-form-item>
           </el-col> -->
-          <el-col :span="8">
-            <el-form-item label="事业部">
-              <IhSelectPageDivision v-model="queryPageParameters.departmentOrgId"></IhSelectPageDivision>
-            </el-form-item>
-          </el-col>
-          <!-- <el-col :span="8">
+        <el-col :span="8">
+          <el-form-item label="事业部">
+            <IhSelectPageDivision v-model="queryPageParameters.departmentOrgId"></IhSelectPageDivision>
+          </el-form-item>
+        </el-col>
+        <!-- <el-col :span="8">
             <el-form-item label="状态">
               <el-select
                 v-model="queryPageParameters.status"
@@ -99,151 +99,151 @@
               </el-select>
             </el-form-item>
           </el-col> -->
-          <el-col :span="8">
-            <el-form-item label="录入人">
-              <IhSelectPageUser
-                v-model="queryPageParameters.inputUser"
-                clearable
-              >
-                <template v-slot="{ data }">
-                  <span style="float: left">{{ data.name }}</span>
-                  <span style="
+        <el-col :span="8">
+          <el-form-item label="录入人">
+            <IhSelectPageUser
+              v-model="queryPageParameters.inputUser"
+              clearable
+            >
+              <template v-slot="{ data }">
+                <span style="float: left">{{ data.name }}</span>
+                <span style="
                       margin-left: 20px;
                       float: right;
                       color: #8492a6;
                       font-size: 13px;
                     ">{{ data.account }}</span>
-                </template>
-              </IhSelectPageUser>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
-            <el-form-item label="特批入库">
-              <el-select
-                v-model="queryPageParameters.special"
-                clearable
-                placeholder="特批入库"
-                class="width--100"
-              >
-                <el-option
-                  v-for="item in $root.dictAllList('YesOrNoType')"
-                  :key="item.code"
-                  :label="item.name"
-                  :value="item.code"
-                ></el-option>
-              </el-select>
-            </el-form-item>
-          </el-col>
-          <el-col :span="8">
-            <el-form-item label="入库编号">
-              <el-input
-                clearable
-                v-model="queryPageParameters.storageNum"
-                placeholder="入库编号"
-              ></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-      </el-form>
-
-
-      <el-row class="el-row">
-        <el-button
-          type="primary"
-          @click="search()"
-        >查询</el-button>
-        <el-button
-          type="info"
-          @click="empty()"
-        >重置</el-button>
+              </template>
+            </IhSelectPageUser>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="特批入库">
+            <el-select
+              v-model="queryPageParameters.special"
+              clearable
+              placeholder="特批入库"
+              class="width--100"
+            >
+              <el-option
+                v-for="item in $root.dictAllList('YesOrNoType')"
+                :key="item.code"
+                :label="item.name"
+                :value="item.code"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="入库编号">
+            <el-input
+              clearable
+              v-model="queryPageParameters.storageNum"
+              placeholder="入库编号"
+            ></el-input>
+          </el-form-item>
+        </el-col>
       </el-row>
+    </el-form>
 
-      <br />
-      <el-table
-        class="ih-table"
-        :data="resPageInfo.list"
-        :default-sort="{ prop: 'id', order: 'descending' }"
-        @selection-change="handleSelectionChange"
-        :empty-text="emptyText"
+    <el-row class="el-row margin-left-100">
+      <el-button
+        type="primary"
+        @click="search()"
+      >查询</el-button>
+      <el-button
+        type="info"
+        @click="empty()"
+      >重置</el-button>
+    </el-row>
+
+    <br />
+    <el-table
+      class="ih-table"
+      :data="resPageInfo.list"
+      :default-sort="{ prop: 'id', order: 'descending' }"
+      @selection-change="handleSelectionChange"
+      :empty-text="emptyText"
+      :max-height="maxHeight"
+    >
+      <el-table-column
+        fixed
+        type="selection"
+        width="50"
+        align="center"
+      ></el-table-column>
+      <el-table-column
+        fixed
+        prop="storageNum"
+        label="入库编号"
+        width="180"
+      ></el-table-column>
+      <el-table-column
+        fixed
+        prop="channelName"
+        label="渠道商名称"
+        width="120"
+      ></el-table-column>
+      <el-table-column
+        prop="province"
+        label="业务开展省份"
+        width="120"
       >
-        <el-table-column
-          fixed
-          type="selection"
-          width="50"
-          align="center"
-        ></el-table-column>
-        <el-table-column
-          fixed
-          prop="storageNum"
-          label="入库编号"
-          width="180"
-        ></el-table-column>
-        <el-table-column
-          fixed
-          prop="channelName"
-          label="渠道商名称"
-          width="100"
-        ></el-table-column>
-        <el-table-column
-          prop="province"
-          label="业务开展省份"
-          width="120"
-        >
-          <template v-slot="{ row }">{{
+        <template v-slot="{ row }">{{
             $root.getAreaName(row.province)
           }}</template>
-        </el-table-column>
-        <el-table-column
-          prop="city"
-          label="业务开展城市"
-          width="120"
-        >
-          <template v-slot="{ row }">{{
+      </el-table-column>
+      <el-table-column
+        prop="city"
+        label="业务开展城市"
+        width="120"
+      >
+        <template v-slot="{ row }">{{
             $root.getAreaName(row.city)
           }}</template>
-        </el-table-column>
-        <el-table-column
-          prop="cityGrade"
-          label="城市等级"
-        >
-          <template v-slot="{ row }">{{
+      </el-table-column>
+      <el-table-column
+        prop="cityGrade"
+        label="城市等级"
+      >
+        <template v-slot="{ row }">{{
             $root.dictAllName(row.cityGrade, "CityLevel")
           }}</template>
-        </el-table-column>
-        <el-table-column
-          prop="channelGrade"
-          label="渠道等级"
-        >
-          <template v-slot="{ row }">{{
+      </el-table-column>
+      <el-table-column
+        prop="channelGrade"
+        label="渠道等级"
+      >
+        <template v-slot="{ row }">{{
             $root.dictAllName(row.channelGrade, "ChannelLevel")
           }}</template>
-        </el-table-column>
-        <el-table-column
-          prop="special"
-          label="特批入库"
-        >
-          <template v-slot="{ row }">{{
+      </el-table-column>
+      <el-table-column
+        prop="special"
+        label="特批入库"
+      >
+        <template v-slot="{ row }">{{
             $root.dictAllName(row.special, "YesOrNoType")
           }}</template>
-        </el-table-column>
-        <el-table-column
-          prop="departmentName"
-          label="事业部"
-        ></el-table-column>
-        <el-table-column
-          prop="inputUserName"
-          label="录入人"
-        ></el-table-column>
-        <el-table-column
-          prop="status"
-          label="状态"
-          width="125"
-        >
-          <template v-slot="{ row }">{{
+      </el-table-column>
+      <el-table-column
+        prop="departmentName"
+        label="事业部"
+      ></el-table-column>
+      <el-table-column
+        prop="inputUserName"
+        label="录入人"
+      ></el-table-column>
+      <el-table-column
+        prop="status"
+        label="状态"
+        width="125"
+      >
+        <template v-slot="{ row }">{{
             $root.dictAllName(row.status, "ChannelGradeStatus")
           }}</template>
-        </el-table-column>
-      </el-table>
+      </el-table-column>
+    </el-table>
 
     <el-pagination
       @size-change="handleSizeChangeMixin"
@@ -264,7 +264,7 @@
         @click="finish()"
         :loading="finishLoading"
       >确 定</el-button>
-    </span>    
+    </span>
   </el-dialog>
 </template>
 
@@ -275,7 +275,7 @@ import { post_customerReportRule_addWXBB } from "@/api/project/index";
 import PaginationMixin from "@/mixins/pagination";
 
 @Component({
-  components: { },
+  components: {},
   mixins: [PaginationMixin],
 })
 export default class UserList extends Vue {
@@ -300,6 +300,14 @@ export default class UserList extends Vue {
     total: null,
     list: [],
   };
+
+  get maxHeight() {
+    let h =
+      (document.documentElement.clientHeight || document.body.clientHeight) -
+      520 +
+      "px";
+    return h;
+  }
 
   editChange(row: any) {
     const DRAFT = row.status === "DRAFT";
@@ -371,11 +379,6 @@ export default class UserList extends Vue {
     this.provinceOption = [];
   }
 
-  // // 添加
-  // add() {
-  //   this.$router.push("/channelLevel/add");
-  // }
-
   update() {
     if (this.selection.length) this.dialogVisible = true;
     else this.$message.warning("请先勾选表格数据");
@@ -421,10 +424,18 @@ export default class UserList extends Vue {
     } else {
       this.$message.warning("请先勾选表格数据");
     }
-  }  
+  }
 }
 </script>
 <style lang="scss" scoped>
+.channelLeveLDialog {
+  /deep/ .el-dialog {
+    margin-top: 4vh !important;
+  }
+  /deep/ .el-dialog__body {
+    padding: 10px 20px;
+  }
+}
 .el-breadcrumb {
   margin-bottom: 20px;
 }
