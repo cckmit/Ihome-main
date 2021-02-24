e0ca07c021b6856d6afd009c6f4bf485697c0a9e
POLYSWEET\zhangyuchi <zhangyuchi@polyihome.com>
2021-02-24 10:32:42 +0800

    tofixed修复v1.2.55

--- ihome/ihome-system/src/views/Home.vue
+++ ihome/ihome-system/src/views/Home.vue
@@ -4,7 +4,7 @@
  * @Author: zyc
  * @Date: 2020-06-22 11:10:43
  * @LastEditors: zyc
- * @LastEditTime: 2021-01-26 17:20:55
+ * @LastEditTime: 2021-02-24 10:04:11
 --> 
 <template>
   <div>
@@ -32,16 +32,20 @@ export default class Home extends Vue {
     label: "path",
   };
   created() {
-    
     this.data = routes;
     let a1 = this.$math.add(0.1, 0.2);
     let a2 = this.$math.sub(0.1, 0.2);
     let a3 = this.$math.multi(0.1, 0.2);
     let a4 = this.$math.div(0.1, 0.2);
-    let a5 = this.$math.tofixed(13.37, 1);
     let a6 = this.$math.addArr([0.1, 0.2, 0.3]);
     let a7 = this.$math.div(10, 2);
-    console.log(a1, a2, a3, a4, a5, a6, a7);
+    console.log(a1, a2, a3, a4, a6, a7);
+    let b1 = this.$math.tofixed(-0.0564, 2);
+    let b2 = this.$math.tofixed(13.37, 1);
+    let b3 = this.$math.tofixed(1.335, 2);
+    let b4 = this.$math.tofixed(-1.355, 2);
+    let b5 = this.$math.tofixed(0.235, 2);
+    console.log(b1, b2, b3, b4, b5);
     // let countChildren = 0;
     // routes.forEach((item: any) => {
     //   if (item.children && item.children.length > 0) {
