e3cbc7a8bcabe550e5f38c10e073ba78385b61af
lushijian17361 <lushijian072@chinasoftinc.com>
2021-03-17 16:11:02 +0800

    1.添加客户的时候加上客户ID 2.判断houseVO字段是否可以编辑增加条件 3.修复reportId问题

--- ihome/ihome-sales/src/views/deal/dealReport/add/entryDealInfo.vue
+++ ihome/ihome-sales/src/views/deal/dealReport/add/entryDealInfo.vue
@@ -766,6 +766,7 @@
     contNoList: any = []; // 分销协议列表---initPage接口
     packageIdsList: any = []; // 收派套餐ids：分销模式---选择分销协议后获取；非分销协议---请求接口获取
     baseInfoByTerm: any = {
+      exMinyuan: null, // 是否明源源：1---是，0---否
       proId: null, // 项目id --- 用于查询分销协议列表
       termId: null, // 项目周期id
       termStageEnum: null, // 项目周期阶段
@@ -1315,13 +1316,24 @@
     * */
     isDisabled(key: any = '', type: any = '') {
       const data: any = this.baseInfoInDeal.myReturnVO;
-      if (!key || !type || !data[type]?.[key]) return false;
-      let flag = false;
-      // 2.对应明源字段是否有值
-      if (data[type][key] && this.postData.roomId) {
-        flag = true;
+      let isMingYuanFlag: any = this.baseInfoByTerm?.exMinyuan === 1;
+      if (!key || !type || !data?.[type]?.[key]) return false;
+      let flag: any = false;
+      // 对应明源字段是否有值
+      if (type === 'houseVO') {
+        // 针对房间vo特殊判断
+        if (data[type][key] && this.postData.roomId && isMingYuanFlag) {
+          flag = true;
+        } else {
+          flag = false;
+        }
       } else {
-        flag = false;
+        // 其他vo的判断
+        if (data[type][key] && this.postData.roomId) {
+          flag = true;
+        } else {
+          flag = false;
+        }
       }
       return flag;
     }
@@ -2086,6 +2098,7 @@
       if (data.length === 0) return;
       let customData: any = {
         addId: data[0].id, // 手动添加的时候保存id --- 为了回显收派金额
+        id: data[0].id,
         cardNo: data[0].certificateNumber,
         cardType: data[0].cardType,
         customerName: data[0].custName,
