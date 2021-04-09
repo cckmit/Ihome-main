529a7dc015f5bbeb7bdc6244c220e16552663e74
lushijian17361 <lushijian072@chinasoftinc.com>
2021-04-09 09:04:02 +0800

    成交列表：添加和业绩申报按钮权限逻辑修改

--- ihome/ihome-sales/src/views/deal/dealReport/list.vue
+++ ihome/ihome-sales/src/views/deal/dealReport/list.vue
@@ -224,11 +224,11 @@
       <el-row>
         <el-button type="primary" @click="getListMixin()">查询</el-button>
         <el-button
-          :class="{ 'ih-data-disabled': hasDealAddAuth('ADD')}"
+          :class="{ 'ih-data-disabled': !hasDealAddAuth('ADD')}"
           v-has="'B.SALES.DEAL.DEALLIST.ADD'"
           type="success" @click="handleAdd('add')">添加</el-button>
         <el-button
-          :class="{ 'ih-data-disabled': hasDealAddAuth('ACHIEVEDECLARE')}"
+          :class="{ 'ih-data-disabled': !hasDealAddAuth('ACHIEVEDECLARE')}"
           v-has="'B.SALES.DEAL.DEALLIST.ACHIEVEDECLARE'"
           type="success" @click="handleAdd('declare')">业绩申报</el-button>
         <el-button type="info" @click="handleReset()">重置</el-button>
@@ -442,12 +442,12 @@
       let flag = true;
       switch (btnName) {
         case "ADD":
-          // 添加 - 平台文员
-          flag = (this as any).$roleTool.RProjectSite();
+          // 添加 - 文员
+          flag = (this as any).$roleTool.RFrontLineClerk();
           break;
         case "ACHIEVEDECLARE":
           // 业绩申报 - 案场
-          flag = (this as any).$roleTool.RFrontLineClerk();
+          flag = (this as any).$roleTool.RProjectSite();
           break;
       }
       return flag;
